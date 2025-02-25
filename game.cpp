f9c55b2962ad7ea8095144636f541b481b961d4a
Bruno <brunodonoso2002@gmail.com>
2025-02-24 18:42:19 -0800

    Updated game.cpp
    Overhaul on getting players' name and to display on terminal

--- game.cpp
+++ game.cpp
@@ -4,17 +4,32 @@ using namespace std;
 
 class Player{
     public: 
-        string PlayerCreation(string name);       
+        string PlayerCreation(string name); // Enter in the name of the player 
+        void playerSubclass(); // Function to help choose what the class the player wants       
+        void DisplayInfo(); 
+
+    private: 
+        string playerName;
+
 };
 
-string Player::PlayerCreation(string name){
-    return name; 
+string Player::PlayerCreation(string name) {
+    playerName = name;
+    return playerName;
+}
+
+void Player::DisplayInfo(){ 
+    cout << playerName; 
 }
 
 int main(){
 
-    Player test;
-    cout << "Your name is: " << test.PlayerCreation("Bruno"); 
+    string name; 
+    cout << "Please enter in your name: ";
+    getline(cin, name);  // Allows full name input
+    Player test; 
+    test.PlayerCreation(name); 
+    test.DisplayInfo(); 
 
     return 0; 
 
